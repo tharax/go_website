a1f1df402da225a5e16ca2e0caf4f38a22fda701
Peter Rosser <tharax@gmail.com>
2016-03-30 21:45:24 +0100

    resolves multiple host addresses

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	host, port, _ := net.SplitHostPort(r.Host)
-	fmt.Fprintf(w, "Hello from %s on port %s, %s", host, port, r.Host)
+	if r.Host == "peterrosser.com" {
+		fmt.Fprintf(w, "Hello from pr")
+	} else if r.Host == "thefirsttrust.org" {
+		fmt.Fprintf(w, "Hello from tft")
+	} else {
+		fmt.Fprintf(w, "Hello from somewhere else")
+	}
 }
 
 func main() {
