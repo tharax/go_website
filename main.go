46b1779115069a2946ee91edd6a000677da08c75
Peter Rosser <tharax@gmail.com>
2016-04-04 22:38:13 +0100

    reduced to simple FileServer

--- main.go
+++ main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Host == "peterrosser.com" {
-		ServePeterRosserWebsite()
-		//fmt.Fprintf(w, "Hello from pr")
-	} else if r.Host == "thefirsttrust.org" {
-		ServeTheFirstTrustWebsite()
-		//fmt.Fprintf(w, "Hello from tft")
-	} else {
-		fmt.Fprintf(w, "Hello from somewhere else")
-	}
-}
+import "net/http"
 
 func main() {
-	http.HandleFunc("/", handler)
-}
-
-func ServePeterRosserWebsite() {
-	log.Fatal(http.ListenAndServe(":80", http.FileServer(http.Dir("/peterrosser"))))
-}
-
-func ServeTheFirstTrustWebsite() {
-	log.Fatal(http.ListenAndServe(":80", http.FileServer(http.Dir("/thefirsttrust"))))
+	panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("/peterrosser"))))
 }
