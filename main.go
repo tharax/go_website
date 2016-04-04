ba1a80cb7952dc9289c98f29defcab9172a09ff6
Peter Rosser <tharax@gmail.com>
2016-04-04 23:14:30 +0100

    Added Port 80; removed email address

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "net/http"
 
 func main() {
+	http.ListenAndServe(":80", http.FileServer(http.Dir("./peterrosser")))
 	http.ListenAndServe(":8080", http.FileServer(http.Dir("./peterrosser")))
 }
