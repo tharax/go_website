bf1a3901ea44a4058dcbf41e5fd972d9cf3d43fa
Peter Rosser <tharax@gmail.com>
2016-04-13 20:06:39 +0100

    Updated referrer link

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import "net/http"
 
 func main() {
-	http.ListenAndServe(":80", http.FileServer(http.Dir("./peterrosser")))
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("./peterrosser")))
+	mux := http.NewServeMux()
+
+	rhpr := http.RedirectHandler("peterrosser.com", 8080)
+	mux.Handle("/peterrosser.com", rhpr)
+	rhtft := http.RedirectHandler("thefirsttrust.org", 8080)
+	mux.Handle("/thefirsttrust.com", rhtft)
+	rhlh := http.RedirectHandler("localhost", 8080)
+	mux.Handle("/localhost", rhlh)
+
+	http.ListenAndServe(":8080", mux)
 }
