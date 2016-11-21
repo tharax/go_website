00a535cb138e9fe09723ec1dec0e62f43c041088
Peter Rosser <tharax@gmail.com>
2016-11-21 15:19:34 +0000

    Trying to get the file server working

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	http.Handle("localhost/", http.FileServer(http.Dir("./localhost")))
 
 	//This adds additional handlers to the default mux.
-	http.Handle("*peterrosser.com/", http.FileServer(http.Dir("./peterrosser")))
+	http.Handle("https://peterrosser.com/", http.FileServer(http.Dir("./peterrosser")))
 	http.Handle("thefirsttrust.org/", http.FileServer(http.Dir("./thefirsttrust")))
 	http.Handle("rosser.software/", http.FileServer(http.Dir("./rossersoftware")))
 	http.Handle("rossersoftware.com/", http.FileServer(http.Dir("./rossersoftware")))
