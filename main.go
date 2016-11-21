0450ece179aa6d343f0c2af22b3b03f30b035bc2
Peter Rosser <tharax@gmail.com>
2016-11-21 20:04:55 +0000

    Trying to get multiple sites working

--- main.go
+++ main.go
@@ -7,19 +7,19 @@ func main() {
 	// redirect every http request to https
 	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
 
-	//This is only here for testing.
-	http.Handle("localhost/", http.FileServer(http.Dir("./localhost")))
-
-	//This adds additional handlers to the default mux.
-	http.Handle("https://peterrosser.com/", http.FileServer(http.Dir("./peterrosser")))
-	http.Handle("thefirsttrust.org/", http.FileServer(http.Dir("./thefirsttrust")))
-	http.Handle("rosser.software/", http.FileServer(http.Dir("./rossersoftware")))
-	http.Handle("rossersoftware.com/", http.FileServer(http.Dir("./rossersoftware")))
-
-	//By using "nil" we use the default mux.
-	http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/peterrosser.com/cert.pem", "/etc/letsencrypt/live/peterrosser.com/privkey.pem", nil)
+	go startSimpleServer("peterrosser.com", "./peterrosser")
+	go startSimpleServer("thefirsttrust.org", "./thefirsttrust")
+	go startSimpleServer("rosser.software", "./rossersoftware")
+	go startSimpleServer("rossersoftware.com", "./rossersoftware")
 }
 
 func redirect(w http.ResponseWriter, req *http.Request) {
 	http.Redirect(w, req, "https://"+req.Host+req.URL.String(), http.StatusMovedPermanently)
+}
+
+func startSimpleServer(serverName, serverFolder string) {
+	//This adds additional handlers to the default mux.
+	http.Handle("/", http.FileServer(http.Dir(serverFolder)))
+	//By using "nil" we use the default mux.
+	http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/"+serverName+"/cert.pem", "/etc/letsencrypt/live/"+serverName+"/privkey.pem", nil)
 }
