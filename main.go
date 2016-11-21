ec20456843729aa1174bf31d7dee2f9cd1e5e304
Peter Rosser <tharax@gmail.com>
2016-11-21 00:55:58 +0000

    Change to listen for HTTPS connections

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import "net/http"
 
 func main() {
+
+	//This is only here for testing.
+	http.Handle("localhost/", http.FileServer(http.Dir("./localhost")))
+
+	//This adds additional handlers to the default mux.
 	http.Handle("peterrosser.com/", http.FileServer(http.Dir("./peterrosser")))
 	http.Handle("thefirsttrust.org/", http.FileServer(http.Dir("./thefirsttrust")))
-	http.Handle("localhost/", http.FileServer(http.Dir("./localhost")))
-	http.ListenAndServe(":80", nil)
+	http.Handle("rosser.software/", http.FileServer(http.Dir("./rossersoftware")))
+	http.Handle("rossersoftware.com/", http.FileServer(http.Dir("./rossersoftware")))
+
+	//By using "nil" we use the default mux.
+	http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/peterrosser.com/cert.pem", "/etc/letsencrypt/live/peterrosser.com/privkey.pem", nil)
 }
