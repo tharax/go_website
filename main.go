db3cddeef95ed08890eb15db4822803a94015811
Peter Rosser <tharax@gmail.com>
2016-11-29 00:25:35 +0000

    Add gorilla/mux package and try with that

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "net/http"
 import "crypto/tls"
 import "log"
+import "github.com/gorilla/mux"
 
 func main() {
 
@@ -16,15 +17,19 @@ func main() {
 	cfg.Certificates = append(cfg.Certificates, getCert("rossersoftware.com"))
 	cfg.BuildNameToCertificate()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("peterrosser.com", peterRosserHandler)
-	mux.HandleFunc("thefirsttrust.org", theFirstTrustHandler)
-	mux.HandleFunc("rossersoftware.com", rosserSoftwareHandler)
-	mux.HandleFunc("rosser.software", rosserSoftwareHandler)
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("peterrosser.com", peterRosserHandler)
+	// mux.HandleFunc("thefirsttrust.org", theFirstTrustHandler)
+	// mux.HandleFunc("rossersoftware.com", rosserSoftwareHandler)
+	// mux.HandleFunc("rosser.software", rosserSoftwareHandler)
+
+	r := mux.NewRouter()
+	r.Host("peterrosser.com").Handler(http.FileServer(http.Dir("./peterrosser")))
+	r.Host("thefirsttrust.org").Handler(http.FileServer(http.Dir("./thefirsttrust")))
 
 	server := http.Server{
 		Addr:      ":443",
-		Handler:   mux,
+		Handler:   r,
 		TLSConfig: cfg,
 	}
 
@@ -32,17 +37,17 @@ func main() {
 
 }
 
-func peterRosserHandler(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir("./peterrosser"))
-}
+// func peterRosserHandler(w http.ResponseWriter, r *http.Request) {
+// 	w = http.FileServer(http.Dir("./peterrosser"))
+// }
 
-func theFirstTrustHandler(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir("./thefirsttrust"))
-}
+// func theFirstTrustHandler(w http.ResponseWriter, r *http.Request) {
+// 	w = http.FileServer(http.Dir("./thefirsttrust"))
+// }
 
-func rosserSoftwareHandler(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir("./rossersoftware"))
-}
+// func rosserSoftwareHandler(w http.ResponseWriter, r *http.Request) {
+// 	w = http.FileServer(http.Dir("./rossersoftware"))
+// }
 
 func getCert(website string) (cert tls.Certificate) {
 	cert, err := tls.LoadX509KeyPair("/etc/letsencrypt/live/"+website+"/cert.pem", "/etc/letsencrypt/live/"+website+"/privkey.pem")
