be8e897688352b5d0e8321632a8039e7b711c23a
Peter Rosser <tharax@gmail.com>
2016-12-12 23:10:11 +0000

    Remove SQL Handler

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main() {
 
 	// create different handlers for different hosts.
 	r := mux.NewRouter()
-	r.Host("peterrosser.com/sql").Handler(http.HandlerFunc(sqlHandler))
 	r.Host("peterrosser.com").Handler(http.FileServer(http.Dir("./peterrosser")))
 	r.Host("thefirsttrust.org").Handler(http.FileServer(http.Dir("./thefirsttrust")))
 	r.Host("rosser.software").Handler(http.FileServer(http.Dir("./rossersoftware")))
